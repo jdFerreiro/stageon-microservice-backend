0316c047ed94e0a82df67ff4690ab9bf5e2feff7
David Ferreiro <fb.jdavid@gmail.com>
2025-09-06 09:00:39 -0400

    fix(identity-service): correct import and usage of RoleController in app module

--- services/identity-service/src/app.module.ts
+++ services/identity-service/src/app.module.ts
@@ -7,7 +7,7 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { AuthService } from './auth/auth.service';
 import { UsersController } from './users/users.controller';
-import { RolesController } from './roles/roles.controller';
+import { RoleController } from './roles/roles.controller';
 import { AuthController } from './auth/auth.controller';
 
 @Module({
@@ -30,6 +30,6 @@ import { AuthController } from './auth/auth.controller';
     AuthModule,
   ],
   providers: [AuthService],
-  controllers: [UsersController, RolesController, AuthController],
+  controllers: [UsersController, RoleController, AuthController],
 })
 export class AppModule {}
